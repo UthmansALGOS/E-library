4b5b75c65466f2bd3cd6fd04b57743fc633cdebf
UthmansALGOS <96424036+UthmansALGOS@users.noreply.github.com>
2023-12-14 19:12:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BottomTabNavigtor from "./components/BottomTabNavigator";
+
+export default class App extends React.Component {
+  render(){
+  return (
+    <BottomTabNavigator/>
+  );
+  }
+}
+
+
